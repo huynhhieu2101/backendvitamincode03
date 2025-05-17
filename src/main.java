0a9f9dd6ab3f444079654b86a4e172238bfa1c58
huynhtrunghieu <huynhtrunghieu@hi-2.local>
2025-05-17 16:30:45 +0700

    add hiu

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.printf("Hello VitaminCode03");
         System.out.printf("Hello ");
-        System.out.printf("Hello aaaaaaa");
+        System.out.printf("Hello ");
+        System.out.println("híu");
     }
 }
